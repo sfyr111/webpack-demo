45127488a44824982f9266fecae03797f3c73441
sfyr111 <sfyr111@163.com>
2018-04-06 00:52:56 +0800

    babelrc & runtime-plugin & polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -11,15 +11,6 @@ module.exports = {
         test: /\.js$/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                targets: {
-                  browsers: ['> 1%', 'last 2 versions']
-                },
-              }],
-            ]
-          },
         },
         exclude: '/node_modules/'
       }
