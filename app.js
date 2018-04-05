e30465c5ea2c2d16799bbf07dd98ff2d09186100
sfyr111 <sfyr111@163.com>
2018-04-06 00:46:23 +0800

    code +

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import 'babel-polyfill'
+
 let func = () => {}
 const n = 1
 let arr = [1, 2, 3]
@@ -6,3 +8,7 @@ let arr2 = arr.map(item => item + 2)
 arr.includes(8)
 
 console.log(new Set(arr2));
+
+function* fn() {
+  
+}
