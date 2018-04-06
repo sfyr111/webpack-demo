b9a3696df8c314a5d075d1b15ca1f45c7a2992ef
sfyr111 <sfyr111@163.com>
2018-04-06 15:25:27 +0800

    stylus

---
+++ stylus/src/app.js
@@ -0,0 +1,32 @@
+/**
+ * style-loader 
+ * style-loader/url
+ */
+// import './css/base.css'
+// import './css/common.css'
+
+/**
+ * style-loader/useable
+ */
+// import base from './css/base.css'
+// import common from './css/common.css'
+
+// base.use()
+// base.unuse()
+
+// var flag = false
+
+// setInterval(() => {
+//   if (flag) base.unuse()
+//   else base.use()
+//   flag = !flag
+// }, 500)
+
+/**
+ * css-module
+ */
+import base from './css/base.styl'
+import common from './css/common.styl'
+
+var app = document.getElementById('app')
+app.innerHTML = `<div class="${base.wrapper}"></div>`
